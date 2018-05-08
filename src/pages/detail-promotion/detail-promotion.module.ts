843845b7cc6b8bccf08e24fe2a555b14719f21a9
Ikkew-san <38241232+Ikkew-san@users.noreply.github.com>
2018-05-09 00:14:29 +0700

    Latest Update

---
+++ src/pages/detail-promotion/detail-promotion.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DetailPromotionPage } from './detail-promotion';
+
+@NgModule({
+  declarations: [
+    DetailPromotionPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DetailPromotionPage),
+  ],
+})
+export class DetailPromotionPageModule {}
